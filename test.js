7e9a2546b4cc1646c478a68ae66ee0238e3e9ade
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-04-26 11:19:56 +0100

    commit

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+
+let name = "Hailey";
+let age = 25;
+let city = "New York";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+let message = "Welcome to JavaScript programming!";
+
+
+console.log("Message length:", message.length);
+
+
+console.log("Uppercase message:", message.toUpperCase());
+
+// Extract and print first three characters
+console.log("First three characters:", message.substring(0, 3));
